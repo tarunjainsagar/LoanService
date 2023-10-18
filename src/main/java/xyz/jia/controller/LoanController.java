8a3f5ab63fbae9011d8d7e435e37fa846225aa79
tarunjainsagar <tarunjainsagar@gmail.com>
2023-10-18 15:24:40 +0530

    fix: enhance queryHistory api with parameters

--- src/main/java/xyz/jia/controller/LoanController.java
+++ src/main/java/xyz/jia/controller/LoanController.java
@@ -2,7 +2,11 @@ package xyz.jia.controller;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
+import xyz.jia.constants.FormatConstants;
 import xyz.jia.model.entity.QueryLog;
 import xyz.jia.model.input.FeeProjectionInput;
 import xyz.jia.model.input.InstallmentProjectionInput;
@@ -11,6 +15,10 @@ import xyz.jia.repository.QueryLogRepository;
 import xyz.jia.service.CalculatorFactory;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
 
 import static xyz.jia.constants.UriConstants.*;
@@ -37,7 +45,21 @@ public class LoanController {
     }
 
     @GetMapping(queryHistoryApi)
-    public List<QueryLog> queryHistory() {
-        return queryLogRepository.findAll();
+    public List<QueryLog> queryHistory(
+            @RequestParam(required = false) @DateTimeFormat(pattern = FormatConstants.dateFormat) Date fromDate,
+            @RequestParam(required = false) @DateTimeFormat(pattern = FormatConstants.dateFormat) Date toDate) {
+
+        if (fromDate != null && toDate != null) {
+            // Return data between fromDate and toDate
+            LocalDateTime fromDateTime = fromDate.toInstant().atZone(ZoneId.systemDefault())
+                    .toLocalDate().atTime(LocalTime.MIN);
+            LocalDateTime toDateTime = toDate.toInstant().atZone(ZoneId.systemDefault())
+                    .toLocalDate().atTime(LocalTime.MAX);
+            return queryLogRepository.findByRequestTimestampBetween(fromDateTime, toDateTime, Sort.by(Sort.Order.desc("requestTimestamp")));
+        } else {
+            // Return the 50 latest records
+            PageRequest pageRequest = PageRequest.of(0, 50, Sort.by(Sort.Order.desc("requestTimestamp")));
+            return queryLogRepository.findAll(pageRequest).getContent();
+        }
     }
 }
--- src/main/java/xyz/jia/repository/QueryLogRepository.java
+++ src/main/java/xyz/jia/repository/QueryLogRepository.java
@@ -1,9 +1,16 @@
 package xyz.jia.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import xyz.jia.model.entity.QueryLog;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface QueryLogRepository extends JpaRepository<QueryLog, Long> {
+
+    List<QueryLog> findByRequestTimestampBetween(LocalDateTime fromDate, LocalDateTime toDate, Sort sort);
+
 }
